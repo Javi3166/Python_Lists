7361f04558eb5149f817860bf4321072684a26c1
Javi3166 <javi3166@yahoo.com>
2024-10-20 22:11:06 -0400

    Added code

---
+++ Main.py
@@ -0,0 +1,16 @@
+'''
+Multi-line
+Comments
+'''
+
+lists_example = ["string", 2, False] #This is fine for Python
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+
+print(friends[1])
+print(friends[-1])
+print(friends[1:])
+print(friends[2:4])
+
+friends[1] = "Mike"
+print(friends[1])
